14e3cd02d00ca8fc5840234373c0abb438879e4e
eunsung <eunsung.shin@gmail.com>
2025-03-19 10:28:31 +0900

    도메인 수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://watanka.github.io',
+  url: 'https://autoblog1.watankah.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/autoblog1.github.io/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
